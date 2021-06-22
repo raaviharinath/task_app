551e8cb783e7d688158ad2ca14bfc19f751e48f7
raaviharinath <raaviharinath@gmail.com>
2021-06-22 12:46:35 +0530

    added code

---
+++ app/javascript/controllers/blueclick_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "stimulus";
+
+
+export default class extends Controller {
+
+	static targets = [ "name" ]
+
+	new() 	{
+   				    const element = this.nameTarget
+
+
+}
